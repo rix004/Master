26f28a31fc076e6680497a6753b979b523260de4
rix004 <rix004@uib.no>
2022-01-12 08:18:49 +0100

    har laget edges-matrisen

--- newbranch.m
+++ newbranch.m
@@ -1,26 +1,37 @@
-function [nodes] = newbranch(x_val,y_val,lev,x1,y1,theta, distance,levels,root_radius)
+function [nodes] = newbranch(x_val,y_val,lev,edges,r_vect,x1,y1,theta, distance,levels,root_radius,rate)
     if levels ~= 1
         x2 = x1+cosd(theta)*distance;
         y2 = y1+sind(theta)*distance;
         l2 = levels-1;
+        dist = sqrt((x2-x1)^2+(y2-y1)^2);
         x_val = [x_val x2];
         y_val = [y_val y2];
         lev = [lev l2];
-        nodes = [x_val' y_val' lev'];       % lev er en kolonne som holder styr på hvilket nivå noden er på.
+        edges = [edges dist];
+        r_vect = [r_vect root_radius];
+        nodes = [x_val' y_val' lev' edges' r_vect'];       % lev er en kolonne som holder styr på hvilket nivå noden er på.
         line([x1 x2],[y1,y2],'LineWidth',root_radius);
-        leftside = newbranch(x_val,y_val,lev,x2,y2,theta+20,distance*0.5,levels-1,root_radius*0.7);
+        
+        leftside = newbranch(x_val,y_val,lev,edges,r_vect,x2,y2,theta+20,distance*0.5,levels-1,root_radius*rate,rate);
+        
         x = leftside(:,1)';
         y = leftside(:,2)';
         z = leftside(:,3)';
-        rightside = newbranch(x,y,z,x2,y2,theta-20,distance*0.5,levels-1,root_radius*0.7);
+        e = leftside(:,4)';
+        r = leftside(:,5)';
+        
+        rightside = newbranch(x,y,z,e,r,x2,y2,theta-20,distance*0.5,levels-1,root_radius*rate,rate);
+        
         x_val = [rightside(:,1)'];
         y_val = [rightside(:,2)'];
         lev = [rightside(:,3)'];
-        nodes = [x_val' y_val' lev'];
+        edges = [rightside(:,4)'];
+        r_vect = [rightside(:,5)'];
+        nodes = [x_val' y_val' lev' edges' r_vect'];
         pause(0.01);
         xlabel('x');
         ylabel('y');
     end
-    nodes = [x_val' y_val' lev'];
+    nodes = [x_val' y_val' lev' edges' r_vect'];
      
 end
--- tree.m
+++ tree.m
@@ -10,17 +10,21 @@ y1 = 0;
 z1 = 0;
 degrees = 90;
 root_radius = 5;
-x_val = [x1];
-y_val = [y1];
-z_val = [z1];
+x = [x1];
+y = [y1];
+z = [z1];
+edges_start = [0];
+radius_start = [0];
+min_rate = 0.7;         % Hvor mye radiusen reduseres med for hvert nivå man går oppover
 
-allnodes = newbranch(x_val,y_val,[levels],x1,y1,degrees,distance,levels,root_radius)
+allnodes = newbranch(x,y,[levels],edges_start,radius_start,x1,y1,degrees,distance,levels,root_radius,min_rate)
 %allnodes3D = newbranch3D(x_val,y_val,z_val,x1,y1,z1,degrees,distance,levels,root_radius);
 %allnodes3D = allnodes3D(length(allnodes3D)+1-num_nodes:end,:)
 
 % Matrise med oversikt over alle nodene. Nodene nummereres fra roten og
 % oppover, levelvis, fra høyre til venstre. 
 nodes = zeros(length(allnodes),3);
+
 p = levels;
 k = 1;
 for j = 1:levels
@@ -34,14 +38,22 @@ for j = 1:levels
     p = p-1;
 end
 
-% Matrise med oversikt over alle kantene
 edges = zeros(num_edges,4);
-edges(1,:)=[sqrt((nodes(2,1)-nodes(1,1))^2+(nodes(2,2)-nodes(1,2))^2) 1 2 root_radius]
-p = 1;
-% for i = 2:num_nodes-1
-%     current_node = i;
-% % Bruke levels på en eller annen måte?
-% edges
+edges(1,:)=[sqrt((nodes(2,1)-nodes(1,1))^2+(nodes(2,2)-nodes(1,2))^2) 1 2 root_radius];
+k=1;
+p = levels;
+for j = 1:levels
+    for i = 2:num_nodes
+        if allnodes(i,3) == p
+            edges(k,1) = allnodes(i,4);
+            edges(k,4) = allnodes(i,5);
+            k = k+1;
+        end
+    end
+    p = p-1;
+end
+
+
     
     
     
